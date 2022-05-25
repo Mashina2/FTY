cafa7c34f572a9548d150a39a59c6d95c121417b
genan566 <avodagbejean@gmail.com>
2022-05-25 11:53:23 +0100

    feat navigations packages

--- App.js
+++ App.js
@@ -8,10 +8,19 @@ import Notification from "./Screens/Notification"
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 // const Stack = createStackNavigator();
+
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
+// const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
@@ -26,6 +35,11 @@ const App = () => {
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Notifications" component={Notification} />
       </Drawer.Navigator>
+
+      {/* <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Notification} />
+      </Tab.Navigator> */}
     </NavigationContainer>
   )
 }
